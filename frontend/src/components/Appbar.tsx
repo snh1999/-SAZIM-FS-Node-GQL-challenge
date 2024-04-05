884b5b939b4293197d9dd1c3804d53c6a8b3a1ba
Nahin <shariarnurnahin99@gmail.com>
2024-04-06 00:38:43 +0600

    added theme switcher

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,5 +1,65 @@
-function AppBar() {
-    return <div>Logo</div>;
+import * as React from "react";
+import { useColorScheme } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Button from "@mui/joy/Button";
+import { Typography } from "@mui/joy";
+
+function ModeToggle() {
+    const { mode, setMode } = useColorScheme();
+    const [mounted, setMounted] = React.useState(false);
+
+    // necessary for server-side rendering
+    // because mode is undefined on the server
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) {
+        return <Button variant="soft">Change Theme</Button>;
+    }
+
+    return (
+        <Button
+            variant="soft"
+            sx={{ margin: 1 }}
+            onClick={() => {
+                setMode(mode === "light" ? "dark" : "light");
+            }}
+        >
+            {mode === "light" ? "Dark Mode" : "Light Mode"}
+        </Button>
+    );
 }
 
-export default AppBar;
+function Logo() {
+    return (
+        <Typography
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+                mr: 2,
+                display: "inline",
+                margin: 1.5,
+                marginLeft: 5,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                fontSize: "1.2rem",
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+            }}
+        >
+            Teebay
+        </Typography>
+    );
+}
+
+export default function MyAppBar() {
+    return (
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Logo />
+            <ModeToggle />
+            <CssBaseline />
+        </div>
+    );
+}
