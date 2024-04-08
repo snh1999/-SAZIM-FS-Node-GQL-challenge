3dfd2319dbf6005efdb56cbeff745f6d91a254c1
Nahin <shariarnurnahin99@gmail.com>
2024-04-09 05:15:05 +0600

    removed redundant datasource config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prismaClient = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
-    },
-});
+export const prismaClient = new PrismaClient();
