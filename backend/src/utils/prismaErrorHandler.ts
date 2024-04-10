86aa5d097512f725acdd11428a8e2733cf44c57e
Nahin <shariarnurnahin99@gmail.com>
2024-04-10 17:32:17 +0600

    added some common prisma erros

--- backend/src/utils/prismaErrorHandler.ts
+++ backend/src/utils/prismaErrorHandler.ts
@@ -9,7 +9,13 @@ export function prismaErrorHandler(callback: Function) {
                 if (error.code === "P2002") {
                     return new AppError(`${error.meta?.target} already exists`, 409);
                 }
-                return;
+                if (error.code === "P2001") {
+                    return new AppError(` Record does not exist`, 404);
+                }
+                if (error.code === "P2011") {
+                    return new AppError(`Make Sure all required fields are filled`, 400);
+                }
+                return new AppError(`Invalid Input`, 409);
             }
             return error;
         });
