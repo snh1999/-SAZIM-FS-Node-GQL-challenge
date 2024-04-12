ac193be0a6ff4eb08ff61a6243069809dc71d14a
Nahin <shariarnurnahin99@gmail.com>
2024-04-12 20:55:33 +0600

    adds token to requests

--- frontend/src/config/apollo_client.ts
+++ frontend/src/config/apollo_client.ts
@@ -1,6 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("token") ?? "";
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    };
+});
 
 export const apolloClient = new ApolloClient({
-  uri: import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000/graphql",
-  cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
 });
