e6520f415173d488946b2fb91791562ede2019bb
Nahin <shariarnurnahin99@gmail.com>
2024-04-13 12:00:10 +0600

    fixed enum selection for edit form

--- frontend/src/pages/components/resuable/form_input/SelectEnum.tsx
+++ frontend/src/pages/components/resuable/form_input/SelectEnum.tsx
@@ -1,5 +1,5 @@
 import { Box, Chip, Option, Select } from "@mui/joy";
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import { InputFeedbackContainer } from "../../containers/InputFeedbackContainer";
 
 interface Props {
@@ -12,39 +12,49 @@ interface Props {
 export default function SelectEnum({ id, placeholder, options, multiple = true }: Readonly<Props>) {
     const {
         formState: { errors },
-        setValue,
-        getValues,
+        control,
     } = useFormContext();
     const error = errors[id]?.message ?? "";
 
+    const emptyValue = multiple ? [] : "";
+
     return (
         <InputFeedbackContainer message={error}>
-            <Select
-                multiple={multiple}
-                defaultValue={getValues(id)}
-                size="lg"
-                placeholder={placeholder}
-                renderValue={(selected) => (
-                    <Box sx={{ display: "flex", gap: "0.5rem" }}>
-                        {selected instanceof Array
-                            ? selected.map((selectedOption) => (
-                                  <Chip key={selectedOption.value} variant="soft" color="primary">
-                                      {selectedOption.label}
-                                  </Chip>
-                              ))
-                            : selected?.label}
-                    </Box>
-                )}
-                onChange={(_, value) => {
-                    setValue(id, value);
+            <Controller
+                name={id}
+                control={control}
+                rules={{ required: error === undefined }}
+                render={({ field }) => {
+                    return (
+                        <Select
+                            size="lg"
+                            multiple={multiple}
+                            value={field.value || emptyValue}
+                            placeholder={placeholder}
+                            onChange={(_, value) => {
+                                field.onChange(value);
+                            }}
+                            renderValue={(selected) => (
+                                <Box sx={{ display: "flex", gap: "0.5rem" }}>
+                                    {selected instanceof Array
+                                        ? selected.map((selectedOption) => (
+                                              <Chip key={selectedOption.value} variant="soft" color="primary">
+                                                  {selectedOption.label}
+                                              </Chip>
+                                          ))
+                                        : selected?.label}
+                                </Box>
+                            )}
+                        >
+                            {Object.entries(options).map(([key, value]) => (
+                                <Option key={key} value={value}>
+                                    {value}
+                                </Option>
+                            ))}
+                        </Select>
+                    );
                 }}
-            >
-                {Object.entries(options).map(([key, value]) => (
-                    <Option key={key} value={value}>
-                        {value}
-                    </Option>
-                ))}
-            </Select>
+            />
         </InputFeedbackContainer>
     );
 }
