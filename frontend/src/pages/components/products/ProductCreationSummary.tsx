a5283abb40ce1cc0f57e0d194fd80ade24fb6589
Nahin <shariarnurnahin99@gmail.com>
2024-05-05 17:32:34 +0600

    bugfix

--- frontend/src/pages/components/products/ProductCreationSummary.tsx
+++ frontend/src/pages/components/products/ProductCreationSummary.tsx
@@ -5,23 +5,23 @@ import { useFormContext } from "react-hook-form";
  * Component for displaying product summary information at the end step of product creation form.
  */
 export default function ProductSummary() {
-    const { getValues } = useFormContext();
+  const { getValues } = useFormContext();
 
-    return (
-        <Box sx={{ letterSpacing: "1px", display: "flex", flexDirection: "column", gap: 1.5, mb: 5 }}>
-            <Typography level="body-md">
-                <b>Title:</b> {getValues("title")}
-            </Typography>
-            <Typography>
-                <b>Categries:</b> {getValues("categories").join(", ")}
-            </Typography>
-            <Typography>
-                <b>Description:</b> {getValues("description")}
-            </Typography>
-            <Typography>
-                <b>Price:</b> ${getValues("price")} <b>To Rent</b>: ${getValues("rentPrice")}{" "}
-                {getValues("rentDuration")}
-            </Typography>
-        </Box>
-    );
+  return (
+    <Box sx={{ letterSpacing: "1px", display: "flex", flexDirection: "column", gap: 1.5, mb: 5 }}>
+      <Typography level="body-md">
+        <b>Title:</b> {getValues("title")}
+      </Typography>
+      <Typography>
+        <b>Categories:</b> {getValues("categories")?.join(", ")}
+      </Typography>
+      <Typography>
+        <b>Description:</b> {getValues("description")}
+      </Typography>
+      <Typography>
+        <b>Price:</b> ${getValues("price")} <b>To Rent</b>: ${getValues("rentPrice")}{" "}
+        {getValues("rentDuration")}
+      </Typography>
+    </Box>
+  );
 }
